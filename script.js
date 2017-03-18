2d3ba9a73aa4ecbef8ffa2e9885eb2eb5738333b
Divya Karunakaran <Divya Karunakaran>
2017-03-19 10:00:16 +1300

    V4- Comment added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Model that handle view and controller
 angular.module('contacts', ['ngRoute'])
   .config(function ($routeProvider) {
     $routeProvider
@@ -10,18 +11,24 @@ angular.module('contacts', ['ngRoute'])
         controller: 'cardCtrl'
       })
   })
+
+//Asynchronously makes a request to the JSON file using http request and handle the response
   .service("getService", function ($http, $log) {
-    var getS = this
-    this.getData = function () {
-      return $http.get('data.json')
-    }
+  var getS = this
+  this.getData = function () {
+  return $http.get('data.json')
+  }
   })
+
+//Controls are Javascript Object, controller handles the data flow in searchRes.html UI
   .controller("searchCtrl", ["$scope", "$log", "$http", "getService",
     function ($scope, $log, $http, getService) {
       getService.getData().then(function (data) {
         $scope.data = data.data
       })
     }])
+
+//Controls the dataflow in card.html UI
   .controller("cardCtrl",
   ["$scope",
     "$routeParams",
@@ -30,9 +37,12 @@ angular.module('contacts', ['ngRoute'])
     function ($scope, $routeParams, getService, $log) {
       var id = $routeParams.id
       getService.getData().then(function (data) {
-        $scope.data = data.data.filter(item => item.id === +id)[0]
+       $scope.data = data.data.filter(item => item.id === +id)[0]
+       //reads the first name and stores in a array
         $scope.data.firstName = $scope.data.name.split(" ")[0];
+       // reads the lastname and stores in a array
         $scope.data.lastName = $scope.data.name.split(" ")[1];
+        //debug the JS object flow in the console
         $log.log($scope.data)
       })
     }])
